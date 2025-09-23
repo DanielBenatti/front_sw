410cf1c610badc190ed61c20f330ab00e2b930c6
DanielBenatti <daniellopesbenatti2@gmail.com>
2025-09-23 12:16:12 -0300

    belongas

--- etecmcm-cadastro-usuarios/src/pages/Home/index.jsx
+++ etecmcm-cadastro-usuarios/src/pages/Home/index.jsx
@@ -2,27 +2,40 @@ import './style.css'
 import Lixeira from '../../assets/lixeira.png'
 
 function Home() {
-  
+  const usuarios = [{
+    id: 1,
+    nome: 'Kevyn',
+    idade: 30,
+    email: 'kevin@email.com'
+  },{
+    id: 2,
+    nome: 'Bryan',
+    idade: 2,
+    email: 'bryan@email.com'
+  }]
   
   return (
    <div className='container'>
-      <form>
+      <form class='fomulario'>
         <h1>Cadastro de Usuários</h1>
         <input name='nome' type="text" />
         <input name='idade' type="number" />
         <input name='email' type="email" />
         <button type='button'>Cadastrar</button>
       </form>
-      <div>
-        <div>
-          <p>Nome: </p>
-          <p>Idade: </p>
-          <p>Email: </p>
+      
+      {usuarios.map(usuario => (
+        <div key={usuario.id} className='card'>
+          <div>
+            <p>Nome: {usuario.nome}</p>
+            <p>Idade: {usuario.idade}</p>
+            <p>Email: {usuario.email}</p>
+          </div>
+          <button>
+            <img src={Lixeira} alt="" />
+          </button>
         </div>
-        <button>
-          <img src={Lixeira} alt="" />
-        </button>
-      </div>
+      ))}
    </div>
   )
 }
